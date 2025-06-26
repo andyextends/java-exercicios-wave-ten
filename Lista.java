3918f3a838d2887c4cf6cccdc7c74b12f1be24b5
andyextends <androamafreitas@gmail.com>
2025-06-26 13:51:04 -0300

    iniciando exercicios pratica de conceitos java

---
+++ Lista.java
@@ -0,0 +1,11 @@
+//Crie um programa que armazene uma lista de pessoas (nome e idade) usando um Map.
+//1. Armazene as informações em um Map<String, Integer>, onde a chave é o nome e o
+//valor é a idade.
+//2. 3. Use um Set para armazenar os nomes das pessoas com mais de 30 anos.
+//Exiba os nomes das pessoas que possuem mais de 30 anos, ordenados em ordem alfabética.
+
+public class Lista {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ NumerosInteiros.java
@@ -0,0 +1,10 @@
+//Crie um programa que receba uma lista de números inteiros do usuário e use streams para:
+//1. Filtrar os números ímpares.
+//2. 3. Dobrar os valores dos números ímpares.
+//Ordenar os números resultados em ordem crescente.
+//4. Exibir a lista final.
+public class NumerosInteiros {
+    public static void main(String[] args) {
+
+    }
+}
