0a5bb4b1a89e6f7e127f9a2a61b78656a50aec4d
andyextends <androamafreitas@gmail.com>
2025-06-27 14:10:09 -0300

    exercio 3  de pratica conceito java

--- NumerosInteiros.java
+++ NumerosInteiros.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 //Crie um programa que receba uma lista de números inteiros do usuário e use streams para:
 //1. Filtrar os números ímpares.
 //2. 3. Dobrar os valores dos números ímpares.
@@ -6,5 +11,23 @@
 public class NumerosInteiros {
     public static void main(String[] args) {
 
-    }
-}
+                Scanner sc = new Scanner(System.in);
+                List<Integer> numeros = new ArrayList<>();
+                System.out.println("Digite números inteiros (digite 0 para encerrar):");
+                while (true) {
+                    int num = sc.nextInt();
+                    if (num == 0) break;
+                    numeros.add(num);
+                }
+                // Processar: filtrar ímpares, dobrar, ordenar e exibir
+                List<Integer> resultado = numeros.stream()
+                        .filter(n -> n % 2 != 0)     // apenas ímpares
+                        .map(n -> n * 2)             // dobrar
+                        .sorted()                    // ordenar crescente
+                        .collect(Collectors.toList());
+                System.out.println("Resultado final:");
+                resultado.forEach(System.out::println);
+            }
+        }
+
+
